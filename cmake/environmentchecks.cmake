aad5c065274986ac9d1723b96a18164c34303098
Alan Garny <agarny@hellix.com>
2021-07-25 07:48:06 +1200

    Detect ClangFormat.

--- cmake/environmentchecks.cmake
+++ cmake/environmentchecks.cmake
@@ -30,7 +30,29 @@ else()
     endif()
 endif()
 
+find_program(CLANG_FORMAT_EXE NAMES ${PREFERRED_CLANG_FORMAT_NAMES} clang-format)
 find_program(CLANG_TIDY_EXE NAMES ${PREFERRED_CLANG_TIDY_NAMES} clang-tidy)
+find_program(GIT_EXE NAMES ${PRFERRED_GIT_NAMES} git)
+
+if(CLANG_FORMAT_EXE AND GIT_EXE)
+    # Note: Git is needed so that we can `git diff` the result of ClangFormat on
+    #       our codebase.
+
+    set(CLANG_FORMAT_MINIMUM_VERSION 12)
+
+    execute_process(COMMAND ${CLANG_FORMAT_EXE} -version
+                    OUTPUT_VARIABLE CLANG_FORMAT_VERSION
+                    OUTPUT_STRIP_TRAILING_WHITESPACE
+                    ERROR_QUIET)
+
+    string(REGEX REPLACE "^.*clang-format version ([.0-9]+).*" "\\1" CLANG_FORMAT_VERSION "${CLANG_FORMAT_VERSION}")
+
+    if(CLANG_FORMAT_VERSION VERSION_LESS CLANG_FORMAT_MINIMUM_VERSION)
+        message(WARNING "ClangFormat ${CLANG_FORMAT_VERSION} was found, but version ${CLANG_FORMAT_MINIMUM_VERSION}+ is needed to run the ClangFormat test.")
+    else()
+        set(CLANG_FORMAT_TESTING_AVAILABLE TRUE CACHE INTERNAL "Executables required to run the ClangFormat test are available.")
+    endif()
+endif()
 
 if(CLANG_TIDY_EXE)
     set(CLANG_TIDY_AVAILABLE TRUE CACHE INTERNAL "Executable required to perform static analysis.")
@@ -51,4 +73,8 @@ else()
     endif()
 endif()
 
-mark_as_advanced(CLANG_TIDY_EXE)
+mark_as_advanced(
+    CLANG_FORMAT_EXE
+    CLANG_TIDY_EXE
+    GIT_EXE
+)
