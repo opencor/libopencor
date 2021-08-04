c8c8be8cca70542e28c9e69d52622a3aa522a4ec
Alan Garny <agarny@hellix.com>
2021-08-04 17:09:29 +1200

    CMake: determine whether Python coverage testing is available.

---
+++ cmake/check_python_packages.py
@@ -0,0 +1,25 @@
+# Copyright libOpenCOR contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import subprocess
+import sys
+
+output = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+installed_packages = [line.decode().split('==')[0] for line in output.split()]
+
+for package in sys.argv[1:]:
+    if package not in installed_packages:
+        sys.exit(1)
+
+sys.exit(0)
