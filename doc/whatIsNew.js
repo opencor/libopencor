3d84a6e817622d46c3cc04c172ab9f982a2428c0
Alan Garny <agarny@hellix.com>
2021-09-24 11:44:26 +1200

    General documentation.

---
+++ doc/whatIsNew.js
@@ -0,0 +1,8 @@
+var jsonData = { "versions": [
+                   { "anchor": "latest", "description": "Latest snapshot", "day": 0, "month": 0, "year": 0,
+                     "categories": [
+                       { "name": "Initial version of libOpenCOR" }
+                     ]
+                   }
+                 ]
+               };
