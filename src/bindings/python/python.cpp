66a2610876981c4fa096546089673141ba419875
Alan Garny <agarny@hellix.com>
2021-10-22 15:05:00 +1300

    Python bindings: some minor cleaning up.

--- src/bindings/python/python.cpp
+++ src/bindings/python/python.cpp
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include "libopencor/version.h"
+#include <libopencor>
 
 #include <pybind11/pybind11.h>
 
