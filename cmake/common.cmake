707b281cf8f2df9a61327c4013e3eaf62adb837d
Alan Garny <agarny@hellix.com>
2021-11-25 13:06:15 +1300

    CMake: create a package for SUNDIALS (#16).

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -163,7 +163,7 @@ function(check_python_package PACKAGE AVAILABLE)
 endfunction()
 
 function(statically_link_third_party_libraries TARGET)
-    foreach(THIRD_PARTY_LIBRARY libCellML libCOMBINE libcurl libNuML libSBML libSEDML)
+    foreach(THIRD_PARTY_LIBRARY libCellML libCOMBINE libcurl libNuML libSBML libSEDML SUNDIALS)
         string(TOUPPER "${THIRD_PARTY_LIBRARY}" THIRD_PARTY_LIBRARY_UC)
 
         target_include_directories(${TARGET} PUBLIC
@@ -173,7 +173,7 @@ function(statically_link_third_party_libraries TARGET)
         add_dependencies(${TARGET} ${THIRD_PARTY_LIBRARY})
 
         target_link_libraries(${TARGET}
-                              PRIVATE ${${THIRD_PARTY_LIBRARY_UC}_LIBRARY})
+                              PRIVATE ${${THIRD_PARTY_LIBRARY_UC}_LIBRARIES})
 
         foreach(DEFINITION ${${THIRD_PARTY_LIBRARY_UC}_DEFINITIONS})
             target_compile_definitions(${TARGET}
