a73024f0a9638cca8a32753dc5958d2245888288
Alan Garny <agarny@hellix.com>
2021-12-01 16:06:07 +1300

    Added a cleanall[.bat] script to clean everything (#59).
    I.e. including the prebuilt directory.

--- clean.bat
+++ clean.bat
@@ -1,9 +1,3 @@
 @ECHO OFF
 
-SETLOCAL ENABLEDELAYEDEXPANSION
-
-TITLE Cleaning libOpenCOR...
-
-SET AppDir=%~dp0
-
-FOR /D %%I IN (_skbuild build src\bindings\python\libopencor.egg-info) DO IF EXIST !AppDir!%%I RMDIR /S /Q !AppDir!%%I
+CALL %~dp0scripts\genericclean
