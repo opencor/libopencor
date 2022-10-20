2e6d632eb872f128b87d9fdef683a6c634de498d
Alan Garny <agarny@hellix.com>
2022-10-20 14:55:59 +1300

    Added some more tests for instantiate() method.

--- tests/bindings/python/test_instantiate_file.py
+++ tests/bindings/python/test_instantiate_file.py
@@ -17,11 +17,29 @@ import utils
 
 
 NON_RETRIEVABLE_LOCAL_FILE = "non_retrievable_file.txt"
+UNKNOWN_LOCAL_FILE = "unknown_file.txt"
+SBML_LOCAL_FILE = "sbml.sbml"
+ERROR_SEDML_LOCAL_FILE = "error.sedml"
+# ---GRY--- TO BE ENABLED WHEN libCellML SUPPORTS CellML 1.X.
+# CELLML_1_X_LOCAL_FILE = "cellml_1_x.cellml"
+SEDML_1_X_LOCAL_FILE = "cellml_1_x.sedml"
+COMBINE_1_X_LOCAL_ARCHIVE = "cellml_1_x.omex"
 CELLML_2_LOCAL_FILE = "cellml_2.cellml"
 SEDML_2_LOCAL_FILE = "cellml_2.sedml"
 COMBINE_2_LOCAL_ARCHIVE = "cellml_2.omex"
 
 NON_RETRIEVABLE_REMOTE_FILE = "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/non_retrievable_file.txt"
+UNKNOWN_REMOTE_FILE = "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/unknown_file.txt"
+SBML_REMOTE_FILE = (
+    "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/sbml.sbml"
+)
+ERROR_SEDML_REMOTE_FILE = (
+    "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/error.sedml"
+)
+# ---GRY--- TO BE ENABLED WHEN libCellML SUPPORTS CellML 1.X.
+# CELLML_1_X_REMOTE_FILE = "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/cellml_1_x.cellml"
+SEDML_1_X_REMOTE_FILE = "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/cellml_1_x.sedml"
+COMBINE_1_X_REMOTE_ARCHIVE = "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/cellml_1_x.omex"
 CELLML_2_REMOTE_FILE = "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/cellml_2.cellml"
 SEDML_2_REMOTE_FILE = "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/cellml_2.sedml"
 COMBINE_2_REMOTE_ARCHIVE = "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/cellml_2.omex"
@@ -31,6 +49,43 @@ def test_instantiate_non_retrievable_local_file():
     f = File(utils.resource_path(NON_RETRIEVABLE_LOCAL_FILE))
 
     assert f.instantiate() == File.Status.NonRetrievableFile
+
+
+def test_instantiate_unknown_local_file():
+    f = File(utils.resource_path(UNKNOWN_LOCAL_FILE))
+
+    assert f.instantiate() == File.Status.NonInstantiableFile
+
+
+def test_instantiate_sbml_local_file():
+    f = File(utils.resource_path(SBML_LOCAL_FILE))
+
+    assert f.instantiate() == File.Status.NonInstantiableFile
+
+
+def test_instantiate_error_sedml_local_file():
+    f = File(utils.resource_path(ERROR_SEDML_LOCAL_FILE))
+
+    assert f.instantiate() == File.Status.NonInstantiableFile
+
+
+# ---GRY--- TO BE ENABLED WHEN libCellML SUPPORTS CellML 1.X.
+# def test_instantiate_cellml_1_x_local_file():
+#     f = File(utils.resource_path(CELLML_1_X_LOCAL_FILE))
+
+#     assert f.instantiate() == File.Status.Ok
+
+
+def test_instantiate_sedml_1_x_local_file():
+    f = File(utils.resource_path(SEDML_1_X_LOCAL_FILE))
+
+    assert f.instantiate() == File.Status.NonInstantiableFile
+
+
+def test_instantiate_combine_1_x_local_archive():
+    f = File(utils.resource_path(COMBINE_1_X_LOCAL_ARCHIVE))
+
+    assert f.instantiate() == File.Status.NonInstantiableFile
 
 
 def test_instantiate_cellml_2_local_file():
@@ -54,7 +109,51 @@ def test_instantiate_combine_2_local_file():
 def test_instantiate_non_retrievable_remote_file():
     f = File(NON_RETRIEVABLE_REMOTE_FILE)
 
+    assert f.resolve() == File.Status.NonRetrievableFile
     assert f.instantiate() == File.Status.NonRetrievableFile
+
+
+def test_instantiate_unknown_remote_file():
+    f = File(UNKNOWN_REMOTE_FILE)
+
+    assert f.resolve() == File.Status.Ok
+    assert f.instantiate() == File.Status.NonInstantiableFile
+
+
+def test_instantiate_sbml_remote_file():
+    f = File(SBML_REMOTE_FILE)
+
+    assert f.resolve() == File.Status.Ok
+    assert f.instantiate() == File.Status.NonInstantiableFile
+
+
+def test_instantiate_error_sedml_remote_file():
+    f = File(ERROR_SEDML_REMOTE_FILE)
+
+    assert f.resolve() == File.Status.Ok
+    assert f.instantiate() == File.Status.NonInstantiableFile
+
+
+# ---GRY--- TO BE ENABLED WHEN libCellML SUPPORTS CellML 1.X.
+# def test_instantiate_cellml_1_x_remote_file():
+#     f = File(CELLML_1_X_REMOTE_FILE)
+
+#     assert f.resolve() == File.Status.Ok
+#     assert f.instantiate() == File.Status.Ok
+
+
+def test_instantiate_sedml_1_x_remote_file():
+    f = File(SEDML_1_X_REMOTE_FILE)
+
+    assert f.resolve() == File.Status.Ok
+    assert f.instantiate() == File.Status.NonInstantiableFile
+
+
+def test_instantiate_combine_1_x_remote_archive():
+    f = File(COMBINE_1_X_REMOTE_ARCHIVE)
+
+    assert f.resolve() == File.Status.Ok
+    assert f.instantiate() == File.Status.NonInstantiableFile
 
 
 def test_instantiate_cellml_2_remote_file():
--- tests/file/instantiatetests.cpp
+++ tests/file/instantiatetests.cpp
@@ -21,11 +21,27 @@ limitations under the License.
 #include <libopencor>
 
 static constexpr const char *NON_RETRIEVABLE_LOCAL_FILE = "non_retrievable_file.txt";
+static constexpr const char *UNKNOWN_LOCAL_FILE = "unknown_file.txt";
+static constexpr const char *SBML_LOCAL_FILE = "sbml.sbml";
+static constexpr const char *ERROR_SEDML_LOCAL_FILE = "error.sedml";
+/*---GRY--- TO BE ENABLED WHEN libCellML SUPPORTS CellML 1.X.
+static constexpr const char *CELLML_1_X_LOCAL_FILE = "cellml_1_x.cellml";
+*/
+static constexpr const char *SEDML_1_X_LOCAL_FILE = "cellml_1_x.sedml";
+static constexpr const char *COMBINE_1_X_LOCAL_ARCHIVE = "cellml_1_x.omex";
 static constexpr const char *CELLML_2_LOCAL_FILE = "cellml_2.cellml";
 static constexpr const char *SEDML_2_LOCAL_FILE = "cellml_2.sedml";
 static constexpr const char *COMBINE_2_LOCAL_ARCHIVE = "cellml_2.omex";
 
 static constexpr const char *NON_RETRIEVABLE_REMOTE_FILE = "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/non_retrievable_file.txt";
+static constexpr const char *UNKNOWN_REMOTE_FILE = "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/unknown_file.txt";
+static constexpr const char *SBML_REMOTE_FILE = "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/sbml.sbml";
+static constexpr const char *ERROR_SEDML_REMOTE_FILE = "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/error.sedml";
+/*---GRY--- TO BE ENABLED WHEN libCellML SUPPORTS CellML 1.X.
+static constexpr const char *CELLML_1_X_REMOTE_FILE = "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/cellml_1_x.cellml";
+*/
+static constexpr const char *SEDML_1_X_REMOTE_FILE = "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/cellml_1_x.sedml";
+static constexpr const char *COMBINE_1_X_REMOTE_ARCHIVE = "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/cellml_1_x.omex";
 static constexpr const char *CELLML_2_REMOTE_FILE = "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/cellml_2.cellml";
 static constexpr const char *SEDML_2_REMOTE_FILE = "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/cellml_2.sedml";
 static constexpr const char *COMBINE_2_REMOTE_ARCHIVE = "https://raw.githubusercontent.com/opencor/libopencor/master/tests/res/cellml_2.omex";
@@ -35,6 +51,50 @@ TEST(InstantiateFileTest, instantiateNonRetrievableLocalFile)
     auto file = libOpenCOR::File::create(libOpenCOR::resourcePath(NON_RETRIEVABLE_LOCAL_FILE));
 
     EXPECT_EQ(file->instantiate(), libOpenCOR::File::Status::NON_RETRIEVABLE_FILE);
+}
+
+TEST(InstantiateFileTest, instantiateUnknownLocalFile)
+{
+    auto file = libOpenCOR::File::create(libOpenCOR::resourcePath(UNKNOWN_LOCAL_FILE));
+
+    EXPECT_EQ(file->instantiate(), libOpenCOR::File::Status::NON_INSTANTIABLE_FILE);
+}
+
+TEST(InstantiateFileTest, instantiateSbmlLocalFile)
+{
+    auto file = libOpenCOR::File::create(libOpenCOR::resourcePath(SBML_LOCAL_FILE));
+
+    EXPECT_EQ(file->instantiate(), libOpenCOR::File::Status::NON_INSTANTIABLE_FILE);
+}
+
+TEST(InstantiateFileTest, instantiateErrorSedmlLocalFile)
+{
+    auto file = libOpenCOR::File::create(libOpenCOR::resourcePath(ERROR_SEDML_LOCAL_FILE));
+
+    EXPECT_EQ(file->instantiate(), libOpenCOR::File::Status::NON_INSTANTIABLE_FILE);
+}
+
+/*---GRY--- TO BE ENABLED WHEN libCellML SUPPORTS CellML 1.X.
+TEST(InstantiateFileTest, instantiateCellml1xLocalFile)
+{
+    auto file = libOpenCOR::File::create(libOpenCOR::resourcePath(CELLML_1_X_LOCAL_FILE));
+
+    EXPECT_EQ(file->instantiate(), libOpenCOR::File::Status::OK);
+}
+*/
+
+TEST(InstantiateFileTest, instantiateSedml1xLocalFile)
+{
+    auto file = libOpenCOR::File::create(libOpenCOR::resourcePath(SEDML_1_X_LOCAL_FILE));
+
+    EXPECT_EQ(file->instantiate(), libOpenCOR::File::Status::NON_INSTANTIABLE_FILE);
+}
+
+TEST(InstantiateFileTest, instantiateCombine1xLocalArchive)
+{
+    auto file = libOpenCOR::File::create(libOpenCOR::resourcePath(COMBINE_1_X_LOCAL_ARCHIVE));
+
+    EXPECT_EQ(file->instantiate(), libOpenCOR::File::Status::NON_INSTANTIABLE_FILE);
 }
 
 TEST(InstantiateFileTest, instantiateCellml2LocalFile)
@@ -62,7 +122,58 @@ TEST(InstantiateFileTest, instantiateNonRetrievableRemoteFile)
 {
     auto file = libOpenCOR::File::create(NON_RETRIEVABLE_REMOTE_FILE);
 
+    EXPECT_EQ(file->resolve(), libOpenCOR::File::Status::NON_RETRIEVABLE_FILE);
     EXPECT_EQ(file->instantiate(), libOpenCOR::File::Status::NON_RETRIEVABLE_FILE);
+}
+
+TEST(InstantiateFileTest, instantiateUnknownRemoteFile)
+{
+    auto file = libOpenCOR::File::create(UNKNOWN_REMOTE_FILE);
+
+    EXPECT_EQ(file->resolve(), libOpenCOR::File::Status::OK);
+    EXPECT_EQ(file->instantiate(), libOpenCOR::File::Status::NON_INSTANTIABLE_FILE);
+}
+
+TEST(InstantiateFileTest, instantiateSbmlRemoteFile)
+{
+    auto file = libOpenCOR::File::create(SBML_REMOTE_FILE);
+
+    EXPECT_EQ(file->resolve(), libOpenCOR::File::Status::OK);
+    EXPECT_EQ(file->instantiate(), libOpenCOR::File::Status::NON_INSTANTIABLE_FILE);
+}
+
+TEST(InstantiateFileTest, instantiateErrorSedmlRemoteFile)
+{
+    auto file = libOpenCOR::File::create(ERROR_SEDML_REMOTE_FILE);
+
+    EXPECT_EQ(file->resolve(), libOpenCOR::File::Status::OK);
+    EXPECT_EQ(file->instantiate(), libOpenCOR::File::Status::NON_INSTANTIABLE_FILE);
+}
+
+/*---GRY--- TO BE ENABLED WHEN libCellML SUPPORTS CellML 1.X.
+TEST(InstantiateFileTest, instantiateCellml1xRemoteFile)
+{
+    auto file = libOpenCOR::File::create(CELLML_1_X_REMOTE_FILE);
+
+    EXPECT_EQ(file->resolve(), libOpenCOR::File::Status::OK);
+    EXPECT_EQ(file->instantiate(), libOpenCOR::File::Status::OK);
+}
+*/
+
+TEST(InstantiateFileTest, instantiateSedml1xRemoteFile)
+{
+    auto file = libOpenCOR::File::create(SEDML_1_X_REMOTE_FILE);
+
+    EXPECT_EQ(file->resolve(), libOpenCOR::File::Status::OK);
+    EXPECT_EQ(file->instantiate(), libOpenCOR::File::Status::NON_INSTANTIABLE_FILE);
+}
+
+TEST(InstantiateFileTest, instantiateCombine1xRemoteArchive)
+{
+    auto file = libOpenCOR::File::create(COMBINE_1_X_REMOTE_ARCHIVE);
+
+    EXPECT_EQ(file->resolve(), libOpenCOR::File::Status::OK);
+    EXPECT_EQ(file->instantiate(), libOpenCOR::File::Status::NON_INSTANTIABLE_FILE);
 }
 
 TEST(InstantiateFileTest, instantiateCellml2RemoteFile)
