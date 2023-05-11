a143382cb11b06d5eab0cad0381e3448ea15cf58
Alan Garny <agarny@hellix.com>
2023-05-11 18:53:23 +1200

    Python version tests: test against the correct version information.

--- tests/bindings/python/test_version.py
+++ tests/bindings/python/test_version.py
@@ -17,80 +17,80 @@ import libopencor as oc
 
 def test_version():
     assert isinstance(oc.version(), int)
-    assert oc.version() > 0
+    assert oc.version() == 0x000100
 
 
 def test_version_string():
     assert isinstance(oc.version_string(), str)
-    assert oc.version_string() != ""
+    assert oc.version_string() == "0.1.0"
 
 
 # ---GRY--- BUILDING_JAVASCRIPT_BINDINGS.
 # def test_clang_version():
 #     assert isinstance(oc.clang_version(), int)
-#     assert oc.clang_version() > 0
+#     assert oc.clang_version() == 0x140003
 
 
 # def test_clang_version_string():
 #     assert isinstance(oc.clang_version_string(), str)
-#     assert oc.clang_version_string() != ""
+#     assert oc.clang_version_string() == "14.0.3"
 
 
 # def test_libcellml_version():
 #     assert isinstance(oc.libcellml_version(), int)
-#     assert oc.libcellml_version() > 0
+#     assert oc.libcellml_version() == 0x000400
 
 
 # def test_libcellml_version_string():
 #     assert isinstance(oc.libcellml_version_string(), str)
-#     assert oc.libcellml_version_string() != ""
+#     assert oc.libcellml_version_string() == "0.4.0"
 
 
 # def test_libcombine_version():
 #     assert isinstance(oc.libcombine_version(), int)
-#     assert oc.libcombine_version() > 0
+#     assert oc.libcombine_version() == 218
 
 
 # def test_libcombine_version_string():
 #     assert isinstance(oc.libcombine_version_string(), str)
-#     assert oc.libcombine_version_string() != ""
+#     assert oc.libcombine_version_string() == "0.2.18"
 
 
 # def test_libcurl_version():
 #     assert isinstance(oc.libcurl_version(), int)
-#     assert oc.libcurl_version() > 0
+#     assert oc.libcurl_version() == 0x075301
 
 
 # def test_libcurl_version_string():
 #     assert isinstance(oc.libcurl_version_string(), str)
-#     assert oc.libcurl_version_string() != ""
+#     assert oc.libcurl_version_string() == "7.83.1-DEV"
 
 
 # def test_libsedml_version():
 #     assert isinstance(oc.libsedml_version(), int)
-#     assert oc.libsedml_version() > 0
+#     assert oc.libsedml_version() == 20032
 
 
 # def test_libsedml_version_string():
 #     assert isinstance(oc.libsedml_version_string(), str)
-#     assert oc.libsedml_version_string() != ""
+#     assert oc.libsedml_version_string() == "2.0.32"
 
 
 # def test_llvm_version():
 #     assert isinstance(oc.llvm_version(), int)
-#     assert oc.llvm_version() > 0
+#     assert oc.llvm_version() == 0x140003
 
 
 # def test_llvm_version_string():
 #     assert isinstance(oc.llvm_version_string(), str)
-#     assert oc.llvm_version_string() != ""
+#     assert oc.llvm_version_string() == "14.0.3"
 
 
 # def test_sundials_version():
 #     assert isinstance(oc.sundials_version(), int)
-#     assert oc.sundials_version() > 0
+#     assert oc.sundials_version() == 0x060200
 
 
 # def test_sundials_version_string():
 #     assert isinstance(oc.sundials_version_string(), str)
-#     assert oc.sundials_version_string() != ""
+#     assert oc.sundials_version_string() == "6.2.0"
