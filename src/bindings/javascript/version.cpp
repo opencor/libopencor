c165efaa3a4262fa9e31dcd7aa412df0d099eeb2
Alan Garny <agarny@hellix.com>
2023-06-30 00:53:42 +1200

    JavaScript: some minor cleaning up.

--- src/bindings/javascript/version.cpp
+++ src/bindings/javascript/version.cpp
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include "libopencor/version.h"
+#include <libopencor>
 
 EMSCRIPTEN_BINDINGS(libOpenCOR_Version)
 {
