c49f38f9a8dc737c2832c7d34ed36fcbb7c764a4
Alan Garny <agarny@hellix.com>
2023-08-15 15:28:08 +1200

    Tests: some minor cleaning up.

--- tests/api/simulation/tests.cmake
+++ tests/api/simulation/tests.cmake
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set(TEST simulation)
+set(TEST seddocument)
 
 list(APPEND TESTS ${TEST})
 
