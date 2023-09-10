e954a4e6c96b62a585e336e76555e75f06e76862
Alan Garny <agarny@hellix.com>
2023-09-11 11:16:39 +1200

    JavaScript and Python: further bindings for SolverProperty.

--- src/bindings/javascript/solver.cpp
+++ src/bindings/javascript/solver.cpp
@@ -46,8 +46,29 @@ void solverApi()
 
     // SolverProperty API.
 
+    emscripten::enum_<libOpenCOR::SolverProperty::Type>("SolverProperty.Type")
+        .value("Boolean", libOpenCOR::SolverProperty::Type::Boolean)
+        .value("Integer", libOpenCOR::SolverProperty::Type::Integer)
+        .value("IntegerGt0", libOpenCOR::SolverProperty::Type::IntegerGt0)
+        .value("IntegerGe0", libOpenCOR::SolverProperty::Type::IntegerGe0)
+        .value("Double", libOpenCOR::SolverProperty::Type::Double)
+        .value("DoubleGt0", libOpenCOR::SolverProperty::Type::DoubleGt0)
+        .value("DoubleGe0", libOpenCOR::SolverProperty::Type::DoubleGe0)
+        .value("List", libOpenCOR::SolverProperty::Type::List);
+
     emscripten::class_<libOpenCOR::SolverProperty>("SolverProperty")
-        .smart_ptr<libOpenCOR::SolverPropertyPtr>("SolverProperty");
+        .smart_ptr<libOpenCOR::SolverPropertyPtr>("SolverProperty")
+        .function("type", &libOpenCOR::SolverProperty::type)
+        .function("name", &libOpenCOR::SolverProperty::name)
+        .function("listValues", &libOpenCOR::SolverProperty::listValues)
+        .function("defaultValue", &libOpenCOR::SolverProperty::defaultValue)
+        .function("hasVoiUnit", &libOpenCOR::SolverProperty::hasVoiUnit);
 
     emscripten::register_vector<libOpenCOR::SolverPropertyPtr>("SolverProperties");
+
+    EM_ASM({
+        Module['SolverProperty']['Type'] = Module['SolverProperty.Type'];
+
+        delete Module['SolverProperty.Type'];
+    });
 }
