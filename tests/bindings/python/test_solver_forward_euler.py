7c34c2dad39710bc2f51130bebd57ee9f4495c02
Alan Garny <agarny@hellix.com>
2023-09-15 14:23:11 +1200

    Tests: use the Forward Euler solver to compute the HH52 model.

--- tests/bindings/python/test_solver_forward_euler.py
+++ tests/bindings/python/test_solver_forward_euler.py
@@ -12,13 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set(TEST solver)
 
-list(APPEND TESTS ${TEST})
+from libopencor import Solver
 
-set(${TEST}_CATEGORY api)
-set(${TEST}_SOURCE_FILES
-    ${CMAKE_CURRENT_LIST_DIR}/basictests.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/coveragetests.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/forwardeulertests.cpp
-)
+
+def test_main():
+    solver = Solver("Forward Euler")
+
+    solver.set_property("Step", "0.01")
