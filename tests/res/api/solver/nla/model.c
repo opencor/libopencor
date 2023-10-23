e8fdf6fe46d00e905189851e69157398b101327e
Alan Garny <602265+agarny@users.noreply.github.com>
2023-10-24 10:20:43 +1300

    NLA resources: changed the NLA system a bit to make it slightly more interesting.

--- tests/res/api/solver/nla/model.c
+++ tests/res/api/solver/nla/model.c
@@ -9,8 +9,8 @@ const size_t VARIABLE_COUNT = 3;
 
 const VariableInfo VARIABLE_INFO[] = {
     {"x", "dimensionless", "my_algebraic_system", ALGEBRAIC},
-    {"y", "dimensionless", "my_algebraic_system", ALGEBRAIC},
-    {"z", "dimensionless", "my_algebraic_system", ALGEBRAIC}
+    {"z", "dimensionless", "my_algebraic_system", ALGEBRAIC},
+    {"y", "dimensionless", "my_algebraic_system", ALGEBRAIC}
 };
 
 double * createVariablesArray()
@@ -44,9 +44,9 @@ void objectiveFunction0(double *u, double *f, void *data)
     variables[1] = u[1];
     variables[2] = u[2];
 
-    f[0] = 2.0*variables[0]+variables[1]-2.0*variables[2]-(-1.0);
-    f[1] = 3.0*variables[0]-3.0*variables[1]-variables[2]-5.0;
-    f[2] = variables[0]-2.0*variables[1]+3.0*variables[2]-6.0;
+    f[0] = variables[0]+variables[2]+variables[1]-2.0;
+    f[1] = 6.0*variables[0]-4.0*variables[2]+5.0*variables[1]-31.0;
+    f[2] = 5.0*variables[0]+2.0*variables[2]+2.0*variables[1]-13.0;
 }
 
 void findRoot0(double *variables)
