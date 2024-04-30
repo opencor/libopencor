171f4c9888ee82325e48b4e07dffab49644a0e4e
Alan Garny <agarny@hellix.com>
2024-05-01 01:42:24 +1200

    Python code formatting.

--- tests/bindings/python/test_sed_run.py
+++ tests/bindings/python/test_sed_run.py
@@ -180,7 +180,8 @@ def test_ode_model_with_no_ode_solver():
     assert_issues(instance, expected_issues)
 
 
-#---GRY--- AS FOR THE ODE MODEL, WE WILL NEED TO ADD AN INTERPRETED VERSION OF THE TEST.
+# ---GRY--- AS FOR THE ODE MODEL, WE WILL NEED TO ADD AN INTERPRETED VERSION OF THE TEST.
+
 
 def test_nla_model():
     expected_issues = [
