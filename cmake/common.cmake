2b760c1111bd45c1d15dab701d01ceafc72bdd8b
Alan Garny <agarny@hellix.com>
2024-05-21 14:57:48 +1200

    CMake: some minor cleaning up.

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -347,7 +347,7 @@ endfunction()
 macro(add_target TARGET)
     add_custom_target(${TARGET} ${ARGN})
 
-    set(TARGETS "${TARGETS};${TARGET}")
+    list(APPEND TARGETS ${TARGET})
 
     list(SORT TARGETS)
 
