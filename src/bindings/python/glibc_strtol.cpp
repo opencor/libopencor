3af817b95b16c4ec29e13b1638b5d5cb0410e130
Alan Garny <agarny@hellix.com>
2026-07-08 17:54:16 +1200

    glibc: split compatibility wrappers.

--- src/bindings/python/glibc_strtol.cpp
+++ src/bindings/python/glibc_strtol.cpp
@@ -14,11 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include <errno.h>
-#include <fcntl.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <unistd.h>
 
 #if defined(__linux__) && defined(__GNUC__)
 // On glibc 2.38+, strtol(), strtoll(), strtoul(), and strtoull() are redirected to __isoc23_strtol(),
@@ -63,104 +60,6 @@ __attribute__((weak)) unsigned long __isoc23_strtoul(const char *pString, char *
 __attribute__((weak)) unsigned long long __isoc23_strtoull(const char *pString, char **pEndPtr, int pBase)
 {
     return strtoull(pString, pEndPtr, pBase);
-}
-}
-
-// arc4random(), arc4random_buf(), and arc4random_uniform() were added in glibc 2.36, so we need to provide a
-// compatibility implementation.
-
-extern "C" {
-__attribute__((weak)) uint32_t arc4random()
-{
-    int fd;
-
-    do {
-        fd = open("/dev/urandom", O_RDONLY | O_CLOEXEC);
-    } while ((fd < 0) && (errno == EINTR));
-
-    if (fd < 0) {
-        __builtin_abort();
-    }
-
-    uint32_t res = 0;
-    size_t offset = 0;
-
-    while (offset < sizeof(res)) {
-        ssize_t bytesRead = read(fd, reinterpret_cast<char *>(&res) + offset, sizeof(res) - offset);
-
-        if (bytesRead > 0) {
-            offset += static_cast<size_t>(bytesRead);
-        } else if ((bytesRead < 0) && (errno == EINTR)) {
-            continue;
-        } else {
-            // EOF or unrecoverable error.
-
-            break;
-        }
-    }
-
-    close(fd);
-
-    if (offset != sizeof(res)) {
-        __builtin_abort();
-    }
-
-    return res;
-}
-
-__attribute__((weak)) void arc4random_buf(void *pBuf, size_t pSize)
-{
-    if (pSize == 0) {
-        return;
-    }
-
-    int fd;
-
-    do {
-        fd = open("/dev/urandom", O_RDONLY | O_CLOEXEC);
-    } while ((fd < 0) && (errno == EINTR));
-
-    if (fd < 0) {
-        __builtin_abort();
-    }
-
-    size_t offset = 0;
-
-    while (offset < pSize) {
-        ssize_t bytesRead = read(fd, static_cast<char *>(pBuf) + offset, pSize - offset);
-
-        if (bytesRead > 0) {
-            offset += static_cast<size_t>(bytesRead);
-        } else if ((bytesRead < 0) && (errno == EINTR)) {
-            continue;
-        } else {
-            // EOF or unrecoverable error.
-
-            break;
-        }
-    }
-
-    close(fd);
-
-    if (offset != pSize) {
-        __builtin_abort();
-    }
-}
-
-__attribute__((weak)) uint32_t arc4random_uniform(uint32_t pUpperBound)
-{
-    if (pUpperBound < 2) {
-        return 0;
-    }
-
-    uint32_t rejectionThreshold = -pUpperBound % pUpperBound;
-    uint32_t number;
-
-    do {
-        number = arc4random();
-    } while (number < rejectionThreshold);
-
-    return number % pUpperBound;
 }
 }
 
